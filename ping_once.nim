bf7c1d4c2e05fe549d7174e183d6ca773c7994f0
CEO <ceo@map2map.com>
2025-06-01 20:04:04 +0530

    Update ping_once.nim

--- ping_once.nim
+++ ping_once.nim
@@ -5,8 +5,11 @@ proc pingPlatform() =
   try:
     let response = client.get("https://platform-krns.onrender.com/")
     echo "Successfully pinged platform at ", $now(), ". Status code: ", $response.status
-  except e:
+  except HttpRequestError as e:
     echo "Error pinging platform at ", $now(), ": ", e.msg
+  except OSError as e:
+    # This will catch any lower-level I/O or socket errors
+    echo "I/O error pinging platform at ", $now(), ": ", e.msg
 
 when isMainModule:
   pingPlatform()
